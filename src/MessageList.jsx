b1c15b1e2cb44b521e070acdb1e89395d783a137
Jinny Huang <jinny921@gmail.com>
2017-05-03 12:09:46 -0700

    chatty app can grab input from chatBar, username and content

---
+++ src/MessageList.jsx
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import Message from './Message.jsx';
+
+class MessageList extends Component {
+  render() {
+    return (
+      <main className='messages'>
+        {this.props.allMessages.map((message)  => {
+          return (
+            <Message message={message} key={message.id}/>
+            );
+        })}
+      </main>
+      );
+    }
+  }
+
+export default MessageList;
